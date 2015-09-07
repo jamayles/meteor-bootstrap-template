7614bc1ca8419b4e2dec716248bbbaecda672870
Andrew Mayles <jamayles@gmail.com>
2015-09-06 18:05:58 -0600

    An implementation of the Bootstrap Jumbotron Template for Meteor

---
+++ client/js/router.js
@@ -0,0 +1,15 @@
+Router.route(
+	'/',
+	{
+		name: 'HomePage',
+		title: 'Jumbotron Template for Bootstrap',
+		metaNames:
+		{
+			viewport: 'width=device-width, initial-scale=1',
+			description: '',
+			author: ''
+		},
+		layoutTemplate: 'MainLayout',
+		template: 'HomePage'
+	}
+);
